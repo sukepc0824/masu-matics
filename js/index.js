f486b0cb40a742cbd447a0d884929eb87ac45fd2
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2024-08-07 01:00:26 +0000

    a

--- js/index.js
+++ js/index.js
@@ -21,11 +21,53 @@ function inflate(val) {
     return val;
 }
 
+class Se {
+    constructor(jparent, src, channels = 1) {
+        const setags = [];
+        for (var i = 0; i < channels; i++) {
+            jparent.append('<audio id="sound" preload="none" src="' + src + '" type="audio/mp3"></audio>');
+            const a = jparent.children('audio:last-child')[0];
+            a.load();
+            setags.push(a);
+        }
+        this._tags = setags;
+        this._channel = 0;
+    }
+    play() {
+        this._channel = (this._channel + 1) % this._tags.length;
+        const a = this._tags[this._channel];
+        if (a.currentTime != 0) {
+            if (!a.ended) {
+                //終わってない場合は再生位置リセット
+                a.currentTime = 0;
+            } else {
+                //終わってたら再生再開
+                a.play();
+            }
+        } else {
+            a.play();
+        }
+    }
+    /** 全てのチャンネルのSEを停止 */
+    stop() {
+        for (var a of this._tags) {
+            if (!a.ended) {
+                a.pause();
+                a.currentTime = 0;
+            }
+        }
+    }
+}
+
 if (localStorage.hasOwnProperty("clear_tables")) {
     clear_tables = JSON.parse(localStorage.getItem("clear_tables"))
 } else {
     clear_tables = []
 }
+
+
+////////////////////////////////////////
+
 
 function lastSelected() {
     if (selected[selected.length - 1] != undefined) {
@@ -260,6 +302,8 @@ class Table {
         this.audio_next.play()
     }
 }
+
+const se = new Se($("body"), "./sounds/select.mp3", 1)
 class TableCel {
     constructor(id) {
         this.id = id
@@ -277,9 +321,7 @@ class TableCel {
             selected.splice(selected.map(obj => obj.id).indexOf(this.id) + 1)
         } else {
             if (selected.length != 0) {
-                this.audio.preload = 'metadata';
-                this.audio.currentTime = 0;
-                this.audio.play()
+                se.play()
             }
             if ($target.hasClass("goal")) {
                 selected.push({
